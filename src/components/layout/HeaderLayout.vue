c256e8d0ffc29c6b4faddfb7a544838ee7223dab
gaizkamg <gaizkamg@gmail.com>
2025-03-11 14:25:48 +0100

    Header cambios

--- src/components/layout/HeaderLayout.vue
+++ src/components/layout/HeaderLayout.vue
@@ -1,13 +1,13 @@
 <template>
   <header class="header-layout">
-    <div class="logo"><img src="@/assets/img/Fintastical-logo-nav-DESKTOP.svg " alt="Fintastical Logo" class="logo" /></div>
+    <div class="logo"><router-link to="/"><img src="@/assets/img/Fintastical-logo-nav-DESKTOP.svg " alt="Fintastical Logo" class="logo" /></router-link></div>
     <nav>
       <ul>
         <li v-if="isAuthenticated"><router-link to="/dashboard">Dashboard</router-link></li>
         <li><router-link to="/blog">Noticias</router-link></li>
         <li><router-link to="/contact">Contacto</router-link></li>
-        <li><router-link to="/auth/login">Login</router-link></li>
-        <li><router-link to="/auth/register">Registro</router-link></li>
+        <li><router-link to="/auth/register">Sign up</router-link></li>
+        <li><router-link to="/auth/login" class="login">Login</router-link></li>
 
         <li v-if="isAuthenticated"><router-link to="/auth/profile">Perfil</router-link></li>
         <li v-if="isAuthenticated"><a href="#" @click.prevent="logout">Logout</a></li>
@@ -37,13 +37,24 @@ const logout = () => {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  background-color: var(--purple-light);
-  color: #fff;
+  background-color: var(--background) ;
+  color: var(--purple-light);
   padding: 10px 20px;
 }
+
 .logo {
 width: 40%;
+min-width: 266px;
+max-width: 500px;
 }
+
+.login {
+  background-color: var(--purple);
+  color: var(--purple-light);
+  padding: 0.5rem 1.2rem;
+  border-radius: 1rem;
+}
+
 nav ul {
   list-style: none;
   display: flex;
@@ -51,8 +62,11 @@ nav ul {
   margin: 0;
   padding: 0;
 }
+
 nav ul li a {
-  color: #fff;
+  color: var(--purple-dark);
   text-decoration: none;
+  font-size: 1.3rem;
 }
+
 </style>
