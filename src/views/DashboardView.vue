e8396f4f89ca5b66c2e6a5a07fb51bdca877ab61
moisescolman <mosscolman64@gmail.com>
2025-03-14 12:17:31 +0100

    After presentacion

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -6,16 +6,16 @@
   <div class="investments-view">
     <h1>Your personal dashboard</h1>
     <InvestmentSummary />
-<!--     <PersonalCharts />
+ <PersonalCharts />
 
-    <InvestmentList /> -->
+
 
   </div>
 </template>
 
 <script setup>
-/* import InvestmentList from '@/components/dashboard/investments/InvestmentList.vue';
-import PersonalCharts from '@/components/charts/PersonalCharts.vue'; */
+
+import PersonalCharts from '@/components/charts/PersonalCharts.vue'; 
 import InvestmentSummary from '@/components/dashboard/investments/InvestmentSummary.vue';
 import Loader from '@/components/LoaderComp.vue'
 
